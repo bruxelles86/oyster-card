03d71349cfee822d06a67270667c14fb6108fdb6
bruxelles86 <w_gant@yahoo.co.uk>
2017-04-18 12:46:25 +0100

    adds in_journey, touch_in, touch_out methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,27 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :in_journey
   DEFAULT_LIMIT = 90
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
     fail "Top-up would exceed £#{DEFAULT_LIMIT} limit" if @balance + amount > DEFAULT_LIMIT
     @balance += amount
   end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,25 +1,49 @@
 require 'oystercard'
 
 describe Oystercard do
+let(:card) { Oystercard.new }
 
   it { is_expected.to respond_to(:balance) }
 
   it 'initializes with a balance of zero' do
-    expect(subject.balance).to eq 0
+    expect(card.balance).to eq 0
   end
 
   it { is_expected.to respond_to(:top_up).with(1).argument }
 
+describe '#touch_in' do
+
+  it 'touches in' do
+    card.touch_in
+    expect(card).to be_in_journey
+  end
+end
+
+describe '#touch_out' do
+
+  it 'touches out' do
+    card.touch_in
+    card.touch_out
+    expect(card).not_to be_in_journey
+  end
+end
+
+describe '#in_journey?' do
+
+  it 'checks the journey status' do
+    expect(card.in_journey?).to eq(true).or(eq(false))
+  end
+end
+
 describe '#top_up' do
   it 'tops up balance by specified amount' do
-    subject.top_up(15)
-    expect(subject.balance).to eq 15
+    card.top_up(15)
+    expect(card.balance).to eq 15
 end
 
   it 'raises an error if top-up would push balance above £90' do
-     expect{ subject.top_up(100) }.to raise_error "Top-up would exceed £#{Oystercard::DEFAULT_LIMIT} limit"
+     expect{ card.top_up(100) }.to raise_error "Top-up would exceed £#{Oystercard::DEFAULT_LIMIT} limit"
   end
-
 end
 
 
